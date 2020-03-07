907f36f2bf9b3d4496d7d0ec4e5fc70cfb1fb554
DarrienG <darrienglasser@outlook.com>
2020-03-06 21:21:59 -0500

    Applets

--- applet-start.sh
+++ applet-start.sh
@@ -1,5 +1,6 @@
 #/usr/bin/env bash
 
+feh --bg-scale ~/developer/dotfiles/walls/soft-gradient.jpg
 redshift -l 42.94:-71.71 & disown > /tmp/no
 autorandr --change
 compton --config ~/.config/compton/compton.conf -b
